e9e50e3bd86639671c1a9417dd2470040f26a7b7
Chou Chamnan <109691277+bestcodAI@users.noreply.github.com>
2024-02-04 21:50:16 +0700

    update security

--- security.js
+++ security.js
@@ -14,3 +14,27 @@ document.onkeydown = (e) => {
  
     return false;
 };
+
+document.addEventListener("keydown", function (e) {
+    if (e.ctrlKey &&
+      (e.keyCode == "61" ||
+        e.keyCode == "107" ||
+        e.keyCode == "173" ||
+        e.keyCode == "109" ||
+        e.keyCode == "187" ||
+        e.keyCode == "189")
+    ) {
+      e.preventDefault();
+    }
+  });
+  document.addEventListener(
+    "wheel",
+    function (e) {
+      if (e.ctrlKey) {
+        e.preventDefault();
+      }
+    },
+    {
+      passive: false
+    }
+  );
